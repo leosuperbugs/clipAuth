ee3e7cc6c127e8ca7c404ae8d271367058d884e4
Mark T. Nie <marktnie@gmail.com>
2018-11-01 20:10:56 +0800

    comment area finished, alpha 0.1 finished

--- action/papercliphack.php
+++ action/papercliphack.php
@@ -22,7 +22,9 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
     var $pdo;
     var $settings;
     var $editperpage;
+    var $replyperpage;
     private $editRecordNum;
+    private $replyRecordNum;
     /**
      * Registers a callback function for a given event
      *
@@ -33,6 +35,7 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
     public function register(Doku_Event_Handler $controller)
     {
         $this->editperpage = $this->getConf('editperpage');
+        $this->replyperpage = $this->getConf('commentperpage');
         require  dirname(__FILE__).'/../settings.php';
         $dsn = "mysql:host=".$this->settings['host'].
             ";dbname=".$this->settings['dbname'].
@@ -139,6 +142,50 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
 </div> 
         ";
     }
+
+    private function replyUnit($replyData, $isFirst) {
+        $pageid = $replyData['pageid'];
+        $needHide = '';
+        if ($isFirst === true) $needHide = 'noshow';
+        $mainPageName = '';
+        $indexForShow = '';
+        if ($pageid) {
+            $indexArray = explode(':', $pageid);
+            $mainPageName = $indexArray[count($indexArray) - 1];
+            $indexForShow = array_reverse($indexArray);
+            $indexForShow = implode('</span>-<span class="paperclip__link">', $indexForShow);
+        }
+        $time   = $replyData['time'];
+        $comment= $replyData['comment'];
+        $replier= $replyData['parentname'];
+        $hash   = $replyData['hash'];
+
+        print "
+<div class='paperclip__reply__unit'>
+    <hr class='paperclip__editlog__split $needHide'>
+    <div class='paperclip__reply__header'>
+        <div class='paperclip__reply__from'>
+            \"$replier\"的回复
+        </div>
+        <div class='paperclip__editlog__time'>
+            $time 
+        </div>
+    </div>
+    <p class='paperclip__editlog__sum'>
+        $comment
+    </p>
+    <div class='paperclip__reply__footer'>
+        <a class='paperclip__editlog__link' href='/doku.php?id=$pageid&#comment_$hash' target='_blank'>查看详情</a>
+        <a class='paperclip__editlog__link' href='/doku.php?id=$pageid&do=show&comment=reply&cid=$hash#discussion__comment_form' target='_blank'>回复</a>
+        <div class='paperclip__editlog__index'>
+            索引：<span class='paperclip__link'>$indexForShow</span>
+        </div>
+    </div>
+</div> 
+        ";
+    }
+
+
     /**
      * Count the total edit record number, not page number.
      * number
@@ -147,6 +194,22 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
         if (isset($this->editRecordNum)) return $this->editRecordNum;
 
         $sql = 'select count(*) from '.$this->settings['editlog']. ' where editor = '."\"".$username."\"";
+        $result = $this->pdo->query($sql);
+
+        if ($result === false) return 1;
+        $num = $result->fetchColumn();
+        return $num;
+    }
+
+    /**
+     * Count the total replies
+     * @param $username
+     * @return int
+     */
+    private  function countReplyForName($username) {
+        if (isset($this->replyRecordNum)) return $this->replyRecordNum;
+
+        $sql = 'select count(*) from '.$this->settings['comment']. ' where parentname ='."\"".$username."\"";
         $result = $this->pdo->query($sql);
 
         if ($result === false) return 1;
@@ -210,7 +273,7 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
     private function editlog($pagenum) {
         // Out put the header part
         $this->printHeader(__CLIP__EDIT__);
-        // Fix me. Here we out put the content of edit history
+        //
         global $USERINFO, $conf;
         $username = $USERINFO['name'];
         $count = $this->countEditForName($username);
@@ -242,23 +305,68 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
         }
     }
 
-
     private function comment($pagenum) {
         // Fix me. Here we out put the content of comment page
         $this->printHeader(__CLIP__COMMENT__);
+
+        // Print the content of replying comment
+        global $USERINFO, $conf;
+        $username = $USERINFO['name'];
+        $count = $this->countReplyForName($username);
+        $pagenum = $this->checkPagenum($pagenum, $count, $username);
+        $offset = ($pagenum - 1) * $this->replyperpage;
+        $countPage = $this->replyperpage;
+
+        $sql = 'select * from '.$this->settings['comment'].' where parentname = :parentname order by time DESC limit :offset, :count';
+        try {
+            $statement = $this->pdo->prepare($sql);
+            $statement->bindValue(':parentname', $username);
+            $statement->bindValue(':offset', $offset, PDO::PARAM_INT);
+            $statement->bindValue(':count', $countPage, PDO::PARAM_INT);
+            $r = $statement->execute();
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+        }
+        $isFirst = true;
+
+        while (($result = $statement->fetch(PDO::FETCH_ASSOC)) !== false) {
+            // Processing the result of editlog, generating a row of log
+            $this->replyUnit($result, $isFirst);
+            $isFirst = false;
+        }
+
+        if ($statement->rowCount() === 0) {
+            echo '您还没有收到回复';
+        }
+
+
+
     }
 
     private function setting() {
         // Fix me. Here we out put the content of user setting
+        $this->printHeader(__CLIP__SETTING__);
 
+        global $USERINFO;
+        $username = $USERINFO['name'];
+        $mail = $USERINFO['mail'];
+        print
+        "
+        <div class='paperclip__settings'>
+        <div class='paperclip__settings__title'>个人信息</div>
+        <div class='paperclip__settings__info'>用户名：$username&nbsp|&nbsp登录邮箱：$mail</div>
+        <a href='/doku.php?do=profile' target='_blank' class='paperclip__settings__update'>前往更改</a>
+</div>
+        ";
     }
 
     /**
      * Print a new cell in the table
      * @param $page
      */
-    private function printOnePagenum ($page) {
-        print "<td class='paperclip__pagenum'><a href='/doku.php?show=editlog&page=$page' class='paperclip__pagehref'>$page</a></td>";
+    private function printOnePagenum ($page, $content) {
+        print "<td class='paperclip__pagenum'><a href='/doku.php?show=$content&page=$page' class='paperclip__pagehref'>$page</a></td>";
     }
 
     private function printPresentPagenum ($page) {
@@ -276,11 +384,11 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
      * @param $start
      * @param $end The range includes the end
      */
-    private function printPageFromRange($start, $end) {
+    private function printPageFromRange($start, $end, $content) {
         if ($start > $end) return;
 
         for ($i = $start; $i <= $end; $i++) {
-            $this->printOnePagenum($i);
+            $this->printOnePagenum($i, $content);
         }
     }
 
@@ -291,7 +399,7 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
      * @param $sum total page number
      * @param $page
      */
-    private function paginationNumber($sum, $page) {
+    private function paginationNumber($sum, $page, $content) {
         // check some exception
         global $USERINFO, $conf;
         $username = $USERINFO['name'];
@@ -307,14 +415,14 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
             $left = $page - 1; // the left part of pagination list
             if ($left <= 4) {
                 if ($left > 0) {
-                    $this->printPageFromRange(1, $left);
+                    $this->printPageFromRange(1, $left, $content);
                 }
             } else {
                 // The table should look like:
                 // 1 ... 4 5
-                $this->printOnePagenum(1);
+                $this->printOnePagenum(1, $content);
                 $this->printEllipsis();
-                $this->printPageFromRange($left - 1, $left);
+                $this->printPageFromRange($left - 1, $left, $content);
             }
             //print centre part
             $this->printPresentPagenum($page);
@@ -322,60 +430,70 @@ class action_plugin_clipauth_papercliphack extends DokuWiki_Action_Plugin
             $right = $sum - $page;
             if ($right <= 4) {
                 if ($right > 0) {
-                    $this->printPageFromRange($page + 1, $sum);
+                    $this->printPageFromRange($page + 1, $sum, $content);
                 }
             } else {
                 // The table should look like:
                 // 7 8 ... 10
-                $this->printPageFromRange($page + 1, $page + 2);
+                $this->printPageFromRange($page + 1, $page + 2, $content);
                 $this->printEllipsis();
-                $this->printOnePagenum($sum);
+                $this->printOnePagenum($sum, $content);
             }
             // print the input and jump button
             print "
 <td class='paperclip__pagejump'>
 <form action='/doku.php' method='get'>
 <input type='text' class='paperclip__pagejump__input' name='page' required>
-<input type='hidden' name='show' value='editlog'> 
+<input type='hidden' name='show' value=$content> 
 <input type='submit' class='paperclip__pagejump__button' value='跳转'>
 </form>
 </td>";
             print "</tr></table></div>";
         }
     }
+
+
+
     public function handle_tpl_content_display(Doku_Event $event, $param)
     {
         global $_GET, $ACT;
         $show = $_GET['show'];
+        global $USERINFO, $conf;
+        $username = $USERINFO['name'];
+        $pagenum = $_GET['page'];
+
         if ($ACT === 'profile' || $show === 'editlog') {
             $event->data = '';
-            global $USERINFO, $conf;
-            $username = $USERINFO['name'];
             // A little bit wired here, need fix
             $editRecordCount = $this->countEditForName($username);
             $sum = ceil($editRecordCount / $this->editperpage);
 
             print "<div class='paperclip__selfinfo'>";
             if ($show === 'editlog') {
-                $pagenum = $_GET['page'];
+                $pagenum = $this->checkPagenum($pagenum, $editRecordCount, $username);
                 $this->editlog($pagenum);
-                $this->paginationNumber($sum, $pagenum);
+                $this->paginationNumber($sum, $pagenum, 'editlog');
             } else {
                 $this->editlog(1);
-                $this->paginationNumber($sum, 1);
+                $this->paginationNumber($sum, 1, 'editlog');
 //                $this->paginationNumber(10, 6);
             }
             print "</div>";
 
         } else if ($show === 'comment') {
             print "<div class='paperclip__selfinfo'>";
-            $pagenum = $_GET['page'];
+            $replyRecordCount = $this->countReplyForName($username);
+            $sum = ceil($replyRecordCount / $this->replyperpage);
+
+            $pagenum = $this->checkPagenum($pagenum, $replyRecordCount, $username);
+            // out putting
             $this->comment($pagenum);
+            $this->paginationNumber($sum, $pagenum, 'comment');
             print "</div>";
             exit;
         } else if ($show === 'setting') {
             print "<div class='paperclip__selfinfo'>";
-            echo 'setting';
+            $this->setting();
             print "</div>";
             exit;
         }
