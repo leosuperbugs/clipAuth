b83e85c2a408c70b853c1af4ca2d542b456caa1b
Ise Lawrence <marktnie@gmail.com>
2019-01-09 16:49:03 +0800

    local fix

--- script.js
+++ script.js
@@ -8,11 +8,4 @@ jQuery( document ).ready(function($) {
             fail(function () {swal("操作失败", "请检查网络连接并重试", "error")});
             ev.preventDefault();
         });
-    $("#dw__register input[name='invitationCode']").on('change', function (ev) {
-            console.log('invt code ajax');
-            $.post('/lib/exe/ajax.php', {invtCode: this, call: 'code'}).
-            done(function () {swal("测试成功", "。。", 'success')}).
-            fail(function () {swal("测试失败", "..", 'error')});
-            ev.preventDefault();
-        });
 });
